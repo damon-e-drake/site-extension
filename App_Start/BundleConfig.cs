206945c6ced1968d27c0b01c34387d91a24acf2b
Damon E. Drake <damon.e.drake@gmail.com>
2016-09-26 16:19:36 -0500

    Updated directory structure for project.

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Excavator {
+  public class BundleConfig {
+    public static void RegisterBundles(BundleCollection bundles) {
+      bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js", "~/Scripts/knockout-{version}.js"));
+      bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+
+      // Add bundle for Knockout Models
+
+      bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css","~/Content/site.css"));
+    }
+  }
+}
