f5a4182441515addf424892897d16ce605235668
Damon E. Drake <damon.e.drake@gmail.com>
2016-09-28 15:01:27 -0500

    Removed scaffolded functions.

--- Controllers/SummaryController.cs
+++ Controllers/SummaryController.cs
@@ -11,26 +11,9 @@ namespace Excavator.Controllers {
     private MongoSearchContext ctx = new MongoSearchContext();
 
     public IHttpActionResult Get() {
-      var domains = ctx.Domains.OrderBy(x => x.Name);
+      var domains = ctx.Domains.Select(x => new { x.Name, x.Documents }).OrderBy(x => x.Name);
       
       return Ok(domains);
-    }
-
-    // GET api/values/5
-    public string Get(int id) {
-      return "value";
-    }
-
-    // POST api/values
-    public void Post([FromBody]string value) {
-    }
-
-    // PUT api/values/5
-    public void Put(int id, [FromBody]string value) {
-    }
-
-    // DELETE api/values/5
-    public void Delete(int id) {
     }
   }
 }
