865ad3511d48b5be9ea162974897c53d7699c919
Damon E. Drake <damon.e.drake@gmail.com>
2016-09-30 07:30:04 -0500

    Creating a Self installer.

--- services/Excavator.Service/Program.cs
+++ services/Excavator.Service/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Excavator.Service {
-  class Program {
-    static void Main(string[] args) {
+  public class Program {
+    public static void Main(string[] args) {
+      //http://stackoverflow.com/questions/4856403/installing-a-windows-service-programatically
+      if (Environment.UserInteractive) {
+        ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+      }
+      Console.WriteLine("Press any key to continue...");
+      Console.ReadKey(true);
     }
   }
 }
